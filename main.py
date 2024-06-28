5ba034e1e60ef51121a9cb25cf8033eb3842894a
Tristan Duyvejonck <tristan.duyvejonck@workiva.com>
2024-06-28 13:09:44 -0700

    Added stuff

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from rbrapi import RocketBotRoyale
 from rbrapi.errors import AuthenticationError, CollectTimedBonusError, LootBoxError
 from logger import Logger
 from time import sleep
+import os
+
+
+def notify(title, text, sound):
+    if sound is not None:
+        os.system(f"afplay {sound}" )
+    os.system("""
+              osascript -e 'display notification "{}" with title "{}"'
+              """.format(text, title))
+
 
 
 def parse_args():
@@ -14,9 +24,10 @@ def parse_args():
     parser.add_argument("--no-logging", action="store_true", help="Disable logging")
     parser.add_argument(
         "--auto-open-crates",
-        action="store_true",
-        help="Automatically open crates if available",
+        type=str,
+        help="Automatically open crates if available. Set value to 1 for regular crates, 2 for premium crates."
     )
+    parser.add_argument("--notify", action="store_true", help="Enable system notifications on macOS")
     return parser.parse_args()
 
 
@@ -40,15 +51,26 @@ def main():
 
             coins = client.account().wallet["coins"]
             logger.info(f"Your coins now: {coins}.")
+            if args.notify:
+                notify("RocketBotRoyale", f"Coins collected successfully. Your coins now: {coins}.", "coin.mp3")
 
-            if args.auto_open_crates and coins >= 1000:
-                award = client.buy_crate()
-                logger.info(f"Crate award is: {award.award_id}")
+            if args.auto_open_crates :
+                if args.auto_open_crates == "1" and coins >= 1000:
+                    award = client.buy_crate()
+                    logger.info(f"Crate award is: {award.award_id}")
+                    if args.notify:
+                        notify("RocketBotRoyale", f"Crate award is: {award.award_id}", "crate.mp3")
+                if args.auto_open_crates == "2" and coins >= 20000:
+                    award = client.buy_crate(elite=True)
+                    logger.info(f"Crate award is: {award.award_id}")
+                    if args.notify:
+                        notify("RocketBotRoyale", f"Crate award is: {award.award_id}", "crate.mp3")
 
         except AuthenticationError as e:
             logger.error(f"Unable to authenticate: {e}")
         except CollectTimedBonusError as e:
             logger.info(f"Bonus not available to claim yet: {e}")
+            notify("RocketBotRoyale", f"Bonus not available to claim yet: {e}", "crate.mp3")
         except LootBoxError as e:
             logger.error(f"Unable to open crates: {e}")
         except Exception as e:
